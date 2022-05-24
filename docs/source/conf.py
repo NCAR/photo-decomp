7edaa906b38404be2338470585e9440ab8ab5a77
Kyle Shores <kyle.shores44@gmail.com>
2022-05-24 12:28:52 -0500

    merging doc and docs, starting version at 0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, NCAR/UCAR'
 author = 'NCAR/UCAR'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = 'stable'
 
 
 # -- General configuration ---------------------------------------------------
