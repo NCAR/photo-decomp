2116166a3b95b933e58e3dd79964d9de44c3a073
Kyle Shores <kyle.shores44@gmail.com>
2022-07-06 15:36:01 -0600

    finishing ch2o test?

--- test/unit/cross_section/ch2o_test.F90
+++ test/unit/cross_section/ch2o_test.F90
@@ -6,18 +6,18 @@
 program test_cross_section
 
   use tuvx_cross_section, only : cross_section_t
-  use tuvx_cross_section_cfc11
+  use tuvx_cross_section_ch2o
   use tuvx_test_cross_section_utils, only : check_values
 
   implicit none
 
-  call test_cross_section_cfc11_t( )
+  call test_cross_section_ch2o_t( )
 
 contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  subroutine test_cross_section_cfc11_t( )
+  subroutine test_cross_section_ch2o_t( )
 
     use musica_assert,                 only : assert
     use musica_constants,              only : dk => musica_dk
@@ -35,27 +35,33 @@ contains
     class(iterator_t), pointer :: iter
     real(kind=dk), allocatable :: results(:,:)
     real(dk), allocatable :: expected(:,:)
-    allocate(expected(10, 6))
+    allocate(expected(15, 6))
 
     ! All of these values were produced by one run of the cross section.
     ! So, these tests are testing that any changes don't produce unexpected
     ! changes. The values here are meaningless.
     expected = reshape([                                                      &
-       1.625e-18, 1.618e-18, 1.612e-18, 1.605e-18, 1.599e-18, 1.593e-18,      &
-       1.349e-18, 1.342e-18, 1.335e-18, 1.328e-18, 1.321e-18, 1.314e-18,      &
-       1.099e-18, 1.091e-18, 1.084e-18, 1.077e-18, 1.070e-18, 1.063e-18,      &
-       8.848e-19, 8.779e-19, 8.710e-19, 8.642e-19, 8.575e-19, 8.507e-19,      &
-       7.140e-19, 7.075e-19, 7.010e-19, 6.946e-19, 6.883e-19, 6.820e-19,      &
-       5.684e-19, 5.624e-19, 5.566e-19, 5.508e-19, 5.451e-19, 5.394e-19,      &
-       4.400e-19, 4.349e-19, 4.298e-19, 4.248e-19, 4.198e-19, 4.149e-19,      &
-       3.338e-19, 3.294e-19, 3.252e-19, 3.209e-19, 3.168e-19, 3.127e-19,      &
-       2.470e-19, 2.435e-19, 2.400e-19, 2.366e-19, 2.332e-19, 2.299e-19,      &
-       1.787e-19, 1.759e-19, 1.731e-19, 1.705e-19, 1.678e-19, 1.652e-19],     &
+       2.847e-20, 2.844e-20, 2.841e-20, 2.839e-20, 2.836e-20, 2.833e-20,      &
+       3.236e-20, 3.234e-20, 3.232e-20, 3.230e-20, 3.228e-20, 3.226e-20,      &
+       3.647e-20, 3.645e-20, 3.644e-20, 3.642e-20, 3.641e-20, 3.639e-20,      &
+       3.373e-20, 3.372e-20, 3.371e-20, 3.370e-20, 3.369e-20, 3.369e-20,      &
+       1.363e-20, 1.362e-20, 1.362e-20, 1.361e-20, 1.360e-20, 1.359e-20,      &
+       2.699e-20, 2.699e-20, 2.699e-20, 2.699e-20, 2.699e-20, 2.699e-20,      &
+       1.979e-20, 1.980e-20, 1.980e-20, 1.981e-20, 1.982e-20, 1.982e-20,      &
+       1.620e-20, 1.618e-20, 1.617e-20, 1.615e-20, 1.613e-20, 1.611e-20,      &
+       1.788e-20, 1.786e-20, 1.783e-20, 1.780e-20, 1.778e-20, 1.775e-20,      &
+       2.349e-21, 2.343e-21, 2.336e-21, 2.330e-21, 2.323e-21, 2.316e-21,      &
+       3.651e-21, 3.645e-21, 3.640e-21, 3.634e-21, 3.629e-21, 3.623e-21,      &
+       3.511e-21, 3.503e-21, 3.495e-21, 3.487e-21, 3.479e-21, 3.471e-21,      &
+       8.750e-23, 8.750e-23, 8.750e-23, 8.750e-23, 8.750e-23, 8.750e-23,      &
+       3.620e-22, 3.620e-22, 3.620e-22, 3.620e-22, 3.620e-22, 3.620e-22,      &
+       3.615e-22, 3.615e-22, 3.615e-22, 3.615e-22, 3.615e-22, 3.615e-22],     &
       (/ size(expected, 2), size(expected, 1) /)                              &
     )
 
+
     ! load test grids
-    call config%from_file( "test/data/grid.190-210.config.json" )
+    call config%from_file( "test/data/grid.300-375.config.json" )
     grids => grid_warehouse_t( config )
 
     ! load test profiles
@@ -63,14 +69,14 @@ contains
     profiles => profile_warehouse_t( config, grids )
 
     ! get cross section config data
-    call config%from_file( "test/data/cross_section.cfc-11.config.json" )
+    call config%from_file( "test/data/cross_section.ch2o.config.json" )
     call config%get( "cross sections", cs_set, Iam )
     iter => cs_set%get_iterator( )
 
     ! load and test cross section w/o extrapolation
     call assert( 101264914, iter%next( ) )
     call cs_set%get( iter, cs_config, Iam )
-    cross_section => cross_section_cfc11_t( cs_config, grids, profiles )
+    cross_section => cross_section_ch2o_t( cs_config, grids, profiles )
     results = cross_section%calculate( grids, profiles )
     call check_values( results, expected, .01_dk )
     deallocate( cross_section )
@@ -78,7 +84,7 @@ contains
     ! load and test cross section with lower extrapolation
     call assert( 101264914, iter%next( ) )
     call cs_set%get( iter, cs_config, Iam )
-    cross_section => cross_section_cfc11_t( cs_config, grids, profiles )
+    cross_section => cross_section_ch2o_t( cs_config, grids, profiles )
     results = cross_section%calculate( grids, profiles )
     call check_values( results, expected, .01_dk )
     deallocate( cross_section )
@@ -86,7 +92,7 @@ contains
     ! load and test cross section with lower and upper extrapolation
     call assert( 101264914, iter%next( ) )
     call cs_set%get( iter, cs_config, Iam )
-    cross_section => cross_section_cfc11_t( cs_config, grids, profiles )
+    cross_section => cross_section_ch2o_t( cs_config, grids, profiles )
     results = cross_section%calculate( grids, profiles )
     call check_values( results, expected, .01_dk )
     deallocate( cross_section )
@@ -97,7 +103,7 @@ contains
     deallocate( profiles )
     deallocate( expected )
 
-  end subroutine test_cross_section_cfc11_t
+  end subroutine test_cross_section_ch2o_t
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
